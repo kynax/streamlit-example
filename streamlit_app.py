652d23af9532c8cfc87d85776f3deb02a88f0e76
kynax <guilemay@gmail.com>
2023-10-18 14:03:57 +0000

    Remove example string

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,6 @@ from datetime import date
 import os.path
 from sports_streams import get_json_output
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 today = date.today()
 today_file = today.strftime("%Y-%m-%d")
 if os.path.isfile(today_file):
