a7c6e78743cdca2b9fa055a804cbe75e9b49351f
kynax <guilemay@gmail.com>
2023-10-18 20:42:26 +0000

    Set page to full width

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+st.set_page_config(layout="wide")
 
 from datetime import date
 import os
