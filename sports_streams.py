425c589e57cb11a05bcc2a1e40134612c4060f75
kynax <guilemay@gmail.com>
2023-10-18 13:55:32 +0000

    Initial commit

---
+++ sports_streams.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_games(sport):
+    nhl_schedule_url = f'https://streameast.top/{sport}/schedule'
+    schedule_html = requests.get(nhl_schedule_url).content
+
+    schedule = BeautifulSoup(schedule_html, 'html.parser')
+    schedule_links = schedule.find_all('a', class_='competition')
+
+    ret = []
+    for l in schedule_links:
+        ret.append( (l.get('title'), l.get('href')) )
+    return ret
+
+def get_streams(game):
+    name = game[0]
+    print(f"Looking for streams for : {name}")
+
+    links = []
+
+    try:
+        game_html = requests.get(game[1]).content
+        game = BeautifulSoup(game_html, 'html.parser')
+
+        buttons = game.find_all('button')
+
+        for b in buttons:
+            link = b.get('datatype')
+            if link is not None:
+                links.append(link)
+
+    except Exception as ex:
+        print('    *** Error in get_stream')
+
+    print(f'    Found {len(links)} streams')
+    return links
+
+def get_hrefs(sports = None):
+    if sports is None:
+        sports = ['nhl','nfl','nba','soccer', 'mlb']
+
+    hrefs = []
+    for s in ['nhl','nfl','nba','soccer', 'mlb']:
+        print(f'**** League {s} ****')
+        for game in get_games(s):
+            links = get_streams(game)
+            if links is not None:
+                hrefs.append( (s, game[0], links) )
+        print('')
+
+    return hrefs
+
+def get_json_output(sports = None):
+    import json
+    hrefs = get_hrefs(sports)
+
+    dd = []
+    for h in hrefs:
+        dd.append({'sport': h[0], 'game': h[1], 'stream': h[2]})
+
+    return json.dumps(dd)
+
+
+def get_html_output(sports = None):
+    hrefs = get_hrefs(sports)
+    if len(hrefs) > 0:
+        with open('all.html', 'w') as f:
+            f.write('<html><body style="font-size: 24px; font-family: sans-serif"><pre>')
+            for s,n,ls in hrefs:
+                try:
+                    f.write(f""" <strong>{s}</strong> {n} """)
+                    for i in range(len(ls)):
+                        f.write(f"""<a href="{ls[i]}">Stream {i}</a>&#9;""")
+                    f.write("""<br/><br/> """)
+                except Exception as ex:
+                    print(f"""Couldn't write {s} {n} to html output""")
+            f.write('</pre></body></html>')
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import math
 import pandas as pd
 import streamlit as st
 
+from datetime import date
+import os.path
+from sports_streams import get_json_output
+
 """
 # Welcome to Streamlit!
 
@@ -14,25 +18,35 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+today = date.today()
+today_file = today.strftime("%Y-%m-%d")
+if os.path.isfile(today_file):
+    st.write( 'Found a stream listing file for ' + today)
+
+try:
+    st.write( get_json_output(['nhl']))
+except Exception as ex:
+    st.write( str(ex))
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if False:
+    with st.echo(code_location='below'):
+        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        Point = namedtuple('Point', 'x y')
+        data = []
 
-    points_per_turn = total_points / num_turns
+        points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        for curr_point_num in range(total_points):
+            curr_turn, i = divmod(curr_point_num, points_per_turn)
+            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+            radius = curr_point_num / total_points
+            x = radius * math.cos(angle)
+            y = radius * math.sin(angle)
+            data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
